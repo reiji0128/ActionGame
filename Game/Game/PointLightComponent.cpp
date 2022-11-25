93f04a1d3801b5e1fd7b26b3ba0712bc3fc40dc6
Nagatomo <reiji128@icloud.com>
2022-11-25 19:11:30 +0900

    値をいじれるように調整

--- Game/Game/PointLightComponent.cpp
+++ Game/Game/PointLightComponent.cpp
@@ -6,13 +6,26 @@
 #include "Mesh.h"
 #include "VertexArray.h"
 
-PointLightComponent::PointLightComponent(GameObject* owner, const Vector3& offset, const Vector3& color)
+PointLightComponent::PointLightComponent(GameObject* owner, const Vector3& offset, const Vector3& color,const float& luminance)
 	:Component(owner)
+	,mLightPos(Vector3::Zero)
 	,mOffset(offset)
 	,mColor(color)
+	,mRadius(0.0f)
+	,mVisible(true)
+	,mLuminance(luminance)
 {
 	GAMEINSTANCE.GetRenderer()->AddPointLight(this);
 	printf("new PointLightComponent : [%5d] owner->( 0x%p )\n", GetID(), owner);
+
+	mLightPos = mOwner->GetPosition() + mOffset;
+
+	const float constant  = 1.0f;  // 定数
+	const float linear    = 0.7f;  // 線形
+	const float quadratic = 1.8f;  // 2乗項
+
+	// ライト減衰半径の計算
+	CalcAttenuationLightRadius(constant, linear, quadratic);
 }
 
 PointLightComponent::~PointLightComponent()
@@ -21,42 +34,51 @@ PointLightComponent::~PointLightComponent()
 	GAMEINSTANCE.GetRenderer()->RemovePointLight(this);
 }
 
+void PointLightComponent::Update(float deltaTime)
+{
+	mLightPos = mOwner->GetPosition() + mOffset;
+}
+
 void PointLightComponent::Draw(Shader* shader, Mesh* sphereMesh)
 {
-	Vector3 lightPos = mOwner->GetPosition() + mOffset;
-	Vector3 ambient = mColor * 0.2f;
+	if (mVisible)
+	{
+		Vector3 ambient = mColor * 0.2f;
 
-	// シェーダーにライトの座標・ディフューズ・アンビエントをセット
-	shader->SetVectorUniform("uLight.position", lightPos);
-	shader->SetVectorUniform("uLight.diffuse" , mColor);
-	shader->SetVectorUniform("uLight.ambient" , ambient);
+		// シェーダーにライトの座標・ディフューズ・アンビエントをセット
+		shader->SetVectorUniform("uLight.position", mLightPos);
+		shader->SetVectorUniform("uLight.diffuse", mColor);
+		shader->SetVectorUniform("uLight.ambient", ambient);
+		shader->SetFloatUniform("luminance", 10.0f);
 
-	// スケール・座標の行列を計算
-	Matrix4 mat = Matrix4::CreateScale(mRadius);
-	mat = mat * Matrix4::CreateTranslation(lightPos);
+		// スケール・座標の行列を計算
+		Matrix4 mat = Matrix4::CreateScale(mRadius);
+		mat = mat * Matrix4::CreateTranslation(mLightPos);
 
-	// シェーダーにモデルの行列をセット
-	shader->SetMatrixUniform("model", mat);
+		// シェーダーにモデルの行列をセット
+		shader->SetMatrixUniform("model", mat);
 
-	// ワールド変換をセット
-	shader->SetMatrixUniform("uWorldTransform", mOwner->GetWorldTransform());
-	// スペキュラ強度セット
-	shader->SetFloatUniform("uSpecPower", 100);
+		// ワールド変換をセット
+		shader->SetMatrixUniform("uWorldTransform", mOwner->GetWorldTransform());
+		// スペキュラ強度セット
+		shader->SetFloatUniform("uSpecPower", 100);
 
-	// 頂点配列をアクティブにセット
-	VertexArray* va = sphereMesh->GetVertexArray();
-	va->SetActive();
+		// 頂点配列をアクティブにセット
+		VertexArray* va = sphereMesh->GetVertexArray();
+		va->SetActive();
 
-	// 描画
-	glDrawElements(GL_TRIANGLES, va->GetNumIndices(), GL_UNSIGNED_INT, nullptr);
+		// 描画
+		glDrawElements(GL_TRIANGLES, va->GetNumIndices(), GL_UNSIGNED_INT, nullptr);
+	}
 }
 
 void PointLightComponent::CalcAttenuationLightRadius(const float constant, const float linear, const float quadratic)
 {
 	// 解の公式より減衰半径を計算
 	float max = std::fmax(std::fmax(mColor.x, mColor.y), mColor.z);
-	float radius = (-linear + std::sqrtf((linear * linear) - 4.0f * quadratic *
-		           (constant - (256.0f / 4.0f) * max))) / (2.0f * quadratic);
+
+	float radius = (-linear + std::sqrtf(linear * linear - 4.0f * quadratic *
+		           (constant - (256.0f / 5.0f) * max))) / (2.0f * quadratic);
 
 	mRadius = radius;
 }
--- Game/Game/PointLightComponent.h
+++ Game/Game/PointLightComponent.h
@@ -11,10 +11,31 @@ class Mesh;
 class PointLightComponent : public Component
 {
 public:
-	PointLightComponent(GameObject* owner,const Vector3& offset, const Vector3& color);
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	/// <param name="owner">コンポーネントを所有するゲームオブジェクト</param>
+	/// <param name="offset">相対座標</param>
+	/// <param name="color">ライトの色</param>
+	/// <param name="luminance">輝度</param>
+	PointLightComponent(GameObject* owner,const Vector3& offset, const Vector3& color,const float& luminance);
 
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
 	~PointLightComponent();
 
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	/// <param name="deltaTime">1フレームの経過時間</param>
+	void Update(float deltaTime) override;
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	/// <param name="shader">使用するシェーダー</param>
+	/// <param name="sphereMesh">描画に使う球体のメッシュ</param>
 	void Draw(Shader* shader, Mesh* sphereMesh);
 
 	/// <summary>
@@ -25,13 +46,37 @@ public:
 	/// <param name="quadratic">2乗項</param>
 	void CalcAttenuationLightRadius(const float constant, const float linear, const float quadratic);
 
+// セッター //
+
+	/// <summary>
+	/// 表示フラグのセット
+	/// </summary>
+	/// <param name="visibleFrag"></param>
+	void SetIsVisible(bool visibleFrag) { mVisible = visibleFrag; }
+
+	/// <summary>
+	/// 色のセット
+	/// </summary>
+	/// <param name="color">色</param>
+	void SetColor(Vector3 color) { mColor = color; }
+
 private:
 
+	// ライトの座標
+	Vector3 mLightPos;
+
+	// オフセット
 	Vector3 mOffset;
 
+	// ポイントライトの色
 	Vector3 mColor;
 
+	// ポイントライトの半径
 	float mRadius;
 
-	bool mIsVisible;
+	// 輝度
+	float mLuminance;
+
+	// 表示フラグ
+	bool mVisible;
 };
